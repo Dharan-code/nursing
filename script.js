011665566aa5548c68091d8dbc2de488eb8d26c8
Dharan-code <cscpktk@gmail.com>
2024-07-25 12:53:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+// let currentSlide = 0;
+// const slides = document.querySelectorAll('.slide');
+// const autoSlideInterval = 1500; // Change slide every 3 seconds (3000ms)
+
+// function nextSlide() {
+//   slides[currentSlide].style.display = 'none';
+//   currentSlide = (currentSlide + 1) % slides.length;
+//   slides[currentSlide].style.display = 'block';
+// }
+
+// // Initial setup
+// slides[currentSlide].style.display = 'block';
+// setInterval(nextSlide, autoSlideInterval);
+
+  
